42bc1b4a11cde09c77668160a5da785ea4db18a5
Guilherme Magno <guimagnorodrigues@gmail.com>
2026-02-24 13:10:17 -0300

    Update dif.c
    Refactor password input logic and add digit extraction.

--- desafio/dif.c
+++ desafio/dif.c
@@ -2,17 +2,39 @@
 #include <math.h>  
 int main() {
 
-    int senha, senhainv, digverif, soma, multiptotal;
+    int senha, senhainv, digverif, soma, multiptotal, temp, tempinv;
+    int centena, dezena, unidade;
 
     printf("Digite a senha de 3 digitos:");
     scanf("%d", &senha);
 
-    if (senha >= 0);
-    do {
-        senhainv = senha %10;
-        printf ("%d", senhainv);
-        senha /= 10;
-    } while (senha != 0);
+	temp = senha;
+	
+	centena = (temp%10*100);
+	dezena = ((temp/10)%10) * 10;
+	unidade = (temp/100);
+	
+	senhainv = centena + dezena + unidade;
+
+    tempinv = senhainv
     
+	soma = senha + senhainv;
+	
+	
+char n1, n2, n3, n4;
+    
+  if(soma =<999){
+  	do{ 
+	n1 = (soma/100)	  
+    n2 = (soma/10)%10
+    n3 = soma %10
+		  
+		  
+	  }
+	  
+	  
+	  
+	  
+  }
     return 0;
 } 
